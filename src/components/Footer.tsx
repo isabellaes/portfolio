a15bfd11ee98d51b0ca6e1f1b3bf8cf5dc285d95
isabellaes <borjessonisabella@gmail.com>
2023-09-30 21:01:10 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import "../style/mobile.css";
 const Footer = () => {
   return (
     <Box sx={{ flexGrow: 1 }} className={"footer"}>
-      <p>Denna hemsida är byggd med React, HTML och CSS.</p>
+      <p>Denna hemsida är byggd med React, Typescript, HTML och CSS.</p>
       <p>&copy; Isabella Ekdahl Sahlberg</p>
     </Box>
   );
