50a6d2b23b30d7a7d22e70e6ecfca1577bb79500
isabellaes <borjessonisabella@gmail.com>
2023-10-05 19:56:58 +0200

    add img to project

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,10 +3,12 @@ import Exarbete2 from "../assets/moodtracker.png";
 import Exarbete3 from "../assets/meditation.png";
 import Exarbete4 from "../assets/Kommaihåg.png";
 import Exarbete5 from "../assets/dagbok.png";
+import bloggImg from "../assets/Skärmbild 2023-10-05 195010.png";
 
 export interface project {
   title: string;
   description: string;
+  descriptionLong?: string;
   linktogit?: string;
   img?: string[];
 }
@@ -37,4 +39,5 @@ export const projectBlog: project = {
   title: "Blogg",
   description: "En enkel blogg byggd med React, Typescript, CSS, HTML",
   linktogit: "https://github.com/isabellaes/blog",
+  img: [bloggImg],
 };
