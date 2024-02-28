d0cf2a1d8c5ec902c281dff367cba0318ec1f30d
isabellaes <borjessonisabella@gmail.com>
2024-02-28 16:45:42 +0100

    remove unused import

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import { Link } from "react-router-dom";
